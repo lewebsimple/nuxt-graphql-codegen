f89c7064dbe8be83e8f88dfe9b303f7b1eee255a
Pascal Martineau <pascal@lewebsimple.ca>
2022-02-28 14:12:53 -0500

    Refactor module structure

--- src/module.ts
+++ src/module.ts
@@ -1,62 +1,63 @@
-import { defineNuxtModule } from '@nuxt/kit'
-import { watch, FSWatcher } from 'chokidar'
-import consola from 'consola'
-import { dirname } from 'pathe'
-import { generate, loadCodegenConfig } from '@graphql-codegen/cli'
+import { defineNuxtModule } from "@nuxt/kit";
+import { watch, FSWatcher } from "chokidar";
+import consola from "consola";
+import { dirname } from "pathe";
+import { generate, loadCodegenConfig } from "@graphql-codegen/cli";
 
 export interface NuxtGraphQLCodegenOptions {
   /**
    * Relative path to the graphql-codegen configuration file.
    * Defaults to the current working directory (i.e. project root).
    */
-  configFilePath?: string
+  configFilePath?: string;
 }
 
-declare module '@nuxt/schema' {
+declare module "@nuxt/schema" {
   interface NuxtConfig {
     graphqlCodegen?: NuxtGraphQLCodegenOptions;
   }
 }
 
-const logger = consola.withScope('nuxt-graphql-codegen')
+const logger = consola.withScope("nuxt-graphql-codegen");
 
 export default defineNuxtModule<NuxtGraphQLCodegenOptions>({
   meta: {
-    name: 'nuxt-graphql-codegen',
-    configKey: 'graphqlCodegen'
+    name: "nuxt-graphql-codegen",
+    configKey: "graphqlCodegen",
   },
   defaults: {
-    configFilePath: process.cwd()
+    configFilePath: process.cwd(),
   },
-  async setup ({ configFilePath }, nuxt) {
-    const { config, filepath } = await loadCodegenConfig({ configFilePath })
-    const cwd = dirname(filepath)
+  async setup({ configFilePath }, nuxt) {
+    const { config, filepath } = await loadCodegenConfig({ configFilePath });
+    const cwd = dirname(filepath);
 
-    async function generateGraphQLCodegen () {
-      const start = Date.now()
-      await generate(Object.assign({ ...config, cwd }), true)
-      const time = Date.now() - start
-      logger.success(`GraphQL Codegen generated code in ${time}ms`)
+    async function generateGraphQLCodegen() {
+      const start = Date.now();
+      await generate(Object.assign({ ...config, cwd }), true);
+      const time = Date.now() - start;
+      logger.success(`GraphQL Codegen generated code in ${time}ms`);
     }
 
     if (nuxt.options.dev) {
-      let watcher: FSWatcher
-      nuxt.hook('build:before', async () => {
+      let watcher: FSWatcher;
+      nuxt.hook("build:before", async () => {
         // TODO: handle multiple schemas / documents
-        const { schema, documents } = config
-        const paths = [schema as string, documents as string].filter(path => !!path && typeof path === 'string')
-        logger.info('GraphQL Codegen watching for changes...')
-        watcher = watch(paths)
-        watcher.on('change', generateGraphQLCodegen)
-        await generateGraphQLCodegen()
-      })
-      nuxt.hook('close', () => watcher.close())
+        const { schema, documents } = config;
+        const paths = [schema as string, documents as string].filter((path) => !!path && typeof path === "string");
+        logger.info("GraphQL Codegen watching for changes...");
+        watcher = watch(paths);
+        watcher.on("change", generateGraphQLCodegen);
+        await generateGraphQLCodegen();
+      });
+      nuxt.hook("close", () => watcher.close());
     } else {
-      nuxt.hook('build:before', async () => {
-        await generateGraphQLCodegen()
-      })
+      nuxt.hook("build:before", async () => {
+        await generateGraphQLCodegen();
+      });
     }
 
-    nuxt.options.build.transpile.push('@graphql-codegen/cli')
-  }
-})
+    nuxt.options.build.transpile ||= [];
+    nuxt.options.build.transpile.push("@graphql-codegen/cli");
+  },
+});
